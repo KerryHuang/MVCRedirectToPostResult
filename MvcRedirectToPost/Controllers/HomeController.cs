9d89404841c781552fc83a08b21d2294445442de
kerry <kerry@obdesign.com.tw>
2017-03-09 14:07:27 +0800

    commit

---
+++ MvcRedirectToPost/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fluentx.Mvc;
+using MvcRedirectToPost.Helper.Results;
+
+namespace MvcRedirectToPost.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // TODO:  加入套件 Fluentx.Mvc
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            //return View();
+
+            // TODO 修改Return方法
+            Dictionary<string, object> postData = new Dictionary<string, object>();
+            postData.Add("id", Guid.NewGuid().ToString());
+            postData.Add("name", "Kerry");
+
+            // TODO 使用 Fluentx.Mvc 方法回傳
+            //return this.RedirectAndPost("http://localhost:53642/Home/Contact", postData);
+
+            // TODO 利用內容物件回傳
+            return Content(new RedirectToPostContentResult("http://localhost:53642/Home/Contact", postData).GetResult());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string id, string name)
+        {
+            ViewBag.Message = string.Format("Your id is {0}. {1}", id, name);
+
+            return View();
+        }
+    }
+}
---
+++ MvcRedirectToPost/Helper/Results/RedirectToPostContentResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRedirectToPost.Helper.Results
+{
+    public class RedirectToPostContentResult : ContentResult
+    {
+        public Dictionary<string, object> PostData { get; set; }
+        public string Url { get; set; }
+        
+        public RedirectToPostContentResult() { }
+        public RedirectToPostContentResult(string url, Dictionary<string, object> postData)
+        {
+            this.Url = url;
+            this.PostData = postData;
+        }
+
+        public virtual string GetResult()
+        {
+            return PreparePOSTForm(this.Url, PostData);
+        }
+
+        /// <summary>
+        /// This method prepares an Html form which holds all data in hidden field in the addetion to form submitting script.
+        /// </summary>
+        /// <param name="url">The destination Url to which the post and redirection will occur, the Url can be in the same App or ouside the App.</param>
+        /// <param name="data">A collection of data that will be posted to the destination Url.</param>
+        /// <returns>Returns a string representation of the Posting form.</returns>
+        /// <Author>Samer Abu Rabie</Author>
+        private static String PreparePOSTForm(string url, Dictionary<string, object> data)
+        {
+            //Set a name for the form
+            string formID = "PostForm";
+
+            //Build the form using the specified data to be posted.
+            StringBuilder strForm = new StringBuilder();
+            strForm.Append("<form id=\"" + formID + "\" name=\"" + formID + "\" action=\"" + url + "\" method=\"POST\">");
+            foreach (KeyValuePair<string, object> pair in data)
+            {
+                strForm.Append("<input type=\"hidden\" name=\"" + pair.Key + "\" value=\"" + pair.Value + "\">");
+            }
+            strForm.Append("</form>");
+
+            //Build the JavaScript which will do the Posting operation.
+            StringBuilder strScript = new StringBuilder();
+            strScript.Append("<script language='javascript'>");
+            strScript.Append("var v" + formID + " = document." + formID + ";");
+            strScript.Append("v" + formID + ".submit();");
+            strScript.Append("</script>");
+
+            //Return the form and the script concatenated. (The order is important, Form then JavaScript)
+            return strForm.ToString() + strScript.ToString();
+        }
+    }
+}
